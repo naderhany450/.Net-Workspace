3df378127e876dcfb18d41926d110d01876c8a13
nader3254 <naderhany638@gmail.com>
2024-12-14 14:41:25 +0200

    first project

---
+++ tutorials_excersizes/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tutorials_excersizes.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
